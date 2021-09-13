83d46f211293c67d97a606c5947fd053dee8927b
Kamil kamzyc Zychowicz <kamzyc@gmail.com>
2021-09-13 21:11:41 +0200

    add correct alert icon

--- src/js/views/alertView.js
+++ src/js/views/alertView.js
@@ -18,39 +18,44 @@ class AlertView extends View {
    }
 
    _createMarkup() {
-      return this._data.data;
+      return `
+      ${this._createIcon()}
+      <p class="alert__text">${this._data.data}</p>`;
    }
 
-   // _createMarkup() {
-   //    switch (this._data) {
-   //       case "ingAdded":
-   //          return "ingAdded";
+   _createIcon() {
+      if (this._data.data === "ingredient removed")
+         return `
+         <svg class="alert__icon" viewBox="0 0 24 24">
+            <path d="M0 0h24v24H0z" fill="none"/>
+            <path d="M19 13H5v-2h14v2z"/>
+         </svg>
+         `;
 
-   //       case "ingRemoved":
-   //          return "ingRemoved";
+      if (this._data.data === "ingredient added")
+         return `
+         <svg class="alert__icon" viewBox="0 0 24 24">
+            <path d="M0 0h24v24H0z" fill="none"/>
+            <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>
+         </svg>
+         `;
 
-   //       case "bookmarkAdded":
-   //          return `
-   //          <svg class="alert__icon" viewBox="0 0 24 24">
-   //             <path
-   //                d="M19,21l-7-3l-7,3V5c0-1.1,0.9-2,2-2l7,0c-0.63,0.84-1,1.87-1,3c0,2.76,2.24,5,5,5c0.34,0,0.68-0.03,1-0.1V21z M17.83,9 L15,6.17l1.41-1.41l1.41,1.41l3.54-3.54l1.41,1.41L17.83,9z" />
-   //          </svg>
-   //          <p class="alert__text">Bookmark added</p>
-   //          `;
+      if (this._data.data === "bookmark added")
+         return `
+         <svg viewBox="0 0 24 24">
+            <rect fill="none" height="24" width="24"/>
+            <path d="M19,21l-7-3l-7,3V5c0-1.1,0.9-2,2-2l7,0c-0.63,0.84-1,1.87-1,3c0,2.76,2.24,5,5,5c0.34,0,0.68-0.03,1-0.1V21z M17.83,9 L15,6.17l1.41-1.41l1.41,1.41l3.54-3.54l1.41,1.41L17.83,9z"/>
+         </svg>
+         `;
 
-   //       case "bookmarkRemoved":
-   //          return `
-   //          <svg class="alert__icon" viewBox="0 0 24 24">
-   //             <rect/>
-   //             <path
-   //                d="M21,7h-6V5h6V7z M19,10.9c-0.32,0.07-0.66,0.1-1,0.1c-2.76,0-5-2.24-5-5c0-1.13,0.37-2.16,1-3L7,3C5.9,3,5,3.9,5,5v16l7-3 l7,3V10.9z" />
-   //          </svg>
-   //          <p class="alert__text">Bookmark removed</p>
-   //          `;
-   //       case "uploaded":
-   //          return "uploaded";
-   //    }
-   // }
+      if (this._data.data === "bookmark removed")
+         return `
+         <svg viewBox="0 0 24 24">
+            <rect fill="none" height="24" width="24"/>
+            <path d="M21,7h-6V5h6V7z M19,10.9c-0.32,0.07-0.66,0.1-1,0.1c-2.76,0-5-2.24-5-5c0-1.13,0.37-2.16,1-3L7,3C5.9,3,5,3.9,5,5v16l7-3 l7,3V10.9z"/>
+         </svg>
+         `;
+   }
 }
 
 export default new AlertView();
