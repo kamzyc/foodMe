9253119adb2a2dcc9c0516648924eb556633ce10
Kamil kamzyc Zychowicz <kamzyc@gmail.com>
2021-09-13 20:59:11 +0200

    add alert status object

--- src/js/config.js
+++ src/js/config.js
@@ -5,3 +5,26 @@ export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const REC_PER_SIDE = 8;
 export const TIMEOUT_SEC = 10;
 export const MODAL_CLOSE_SEC = 1;
+export const ALERT_CLOSE_SEC = 1;
+
+export const ALERT_STATUS = {
+   ADD_BOOKMARK: {
+      data: "bookmark added",
+      color: "green",
+   },
+
+   REMOVE_BOOKMARK: {
+      data: "bookmark removed",
+      color: "red",
+   },
+
+   ADD_ING: {
+      data: "ingredient added",
+      color: "green",
+   },
+
+   REMOVE_ING: {
+      data: "ingredient removed",
+      color: "red",
+   },
+};
