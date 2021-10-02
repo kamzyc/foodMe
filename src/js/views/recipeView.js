86f10ceac4516eabb35a28fc297c8fb3bffc5931
Kamil kamzyc Zychowicz <kamzyc@gmail.com>
2021-10-02 16:50:56 +0200

    change user recipe icon

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -79,8 +79,18 @@ class RecipieView extends View {
    _createTitleMarkup() {
       return `
       <figure class="title">
+         <div class="title__user">
+            <svg class="title__icon title__icon--user${
+               this._data.key ? "-active" : ""
+            }" viewBox="0 0 24 24">
+               <path
+                  d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z" />
+            </svg>
+         </div>
          <h1 class="title__text">${this._data.title}</h1>
-         <img src="${this._data.imageUrl}" class="title__img" alt="${this._data.title}" crossOrigin = "anonymous">
+         <img src="${this._data.imageUrl}" class="title__img" alt="${
+         this._data.title
+      }" crossOrigin = "anonymous">
       </figure>
       `;
    }
@@ -136,15 +146,6 @@ class RecipieView extends View {
                   </svg>
                </button>
             </div>
-         </div>
-
-         <div class="detail">
-            <svg class="detail__icon detail__icon--user${
-               this._data.key ? "-active" : ""
-            }" viewBox="0 0 24 24">
-               <path
-                  d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z" />
-            </svg>
          </div>
 
          <button class="btn btn-bookmark ${
